61150b350010849e78051f22f8a568f7091d232e
RUBO <52628935+losehu@users.noreply.github.com>
2025-01-13 14:39:13 +0800

    Update find_data.sh

--- find_data.sh
+++ find_data.sh
@@ -1,22 +1,3 @@
-ps@ps-60t:~$ cat /mnt/nfs_b/CVPU_SERVER/find_data.sh 
-USER_NAME=$(whoami)
-sudo su <<EOF
-echo "$USER_NAME"
-
-docker_output=\$(docker ps -a --format '{{.Status}} {{.Names}}')
-echo "\$docker_output" | while IFS= read -r line; do
-    # 提取状态和容器名
-    DOCKER_STATUS=\$(echo "\$line" | awk '{for(i=1;i<=NF-1;i++) printf \$i " ";}')
-    DOCKER_NAME=\$(echo "\$line" | awk '{print \$NF}')
-    DOCKER_PATH=\$(docker inspect "\$DOCKER_NAME" | grep -o '"Source": "[^"]*-storage"' | awk -F': ' '{print \$2}' | tr -d '"' | head -n 1)
-    if [ -d "\$DOCKER_PATH" ]; then
-        DOCKER_SIZE=\$(du -sh "\$DOCKER_PATH" | awk '{print \$1}')
-    else
-        DOCKER_SIZE="N/A"
-    fi
-
-    
-ps@ps-60t:~$ cat /mnt/nfs_b/CVPU_SERVER/find_data.sh 
 USER_NAME=$(whoami)
 sudo su <<EOF
 echo "$USER_NAME"
